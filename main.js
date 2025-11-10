d9abf7c697cabb658078b350c9c9054429cd4639
kristijanxo <145933386+kristijanxo@users.noreply.github.com>
2025-11-10 18:27:36 +0100

    Create functions and event listeners to populate screen with digit input

--- main.js
+++ main.js
@@ -1,3 +1,58 @@
+const screenTopRow = document.querySelector(".screen-top-row");
+const screenBottomRow = document.querySelector(".screen-bottom-row");
+const buttonContainer = document.querySelector(".button-container");
+
+buttonContainer.addEventListener("click", (event) => {
+  let target = event.target;
+
+  switch (target.id) {
+    case "zero":
+      appendInput(0);
+      break;
+
+    case "one":
+      appendInput(1);
+      break;
+
+    case "two":
+      appendInput(2);
+      break;
+
+    case "three":
+      appendInput(3);
+      break;
+
+    case "four":
+      appendInput(4);
+      break;
+
+    case "five":
+      appendInput(5);
+      break;
+
+    case "six":
+      appendInput(6);
+      break;
+
+    case "seven":
+      appendInput(7);
+      break;
+
+    case "eight":
+      appendInput(8);
+      break;
+
+    case "nine":
+      appendInput(9);
+      break;
+
+    case "backspace":
+      inputArray.pop();
+      updateScreenContent();
+      break;
+  }
+});
+
 function add(a, b) {
   return a + b;
 }
@@ -29,6 +84,17 @@ function operate(operator, a, b) {
       return divide(a, b);
   }
 }
+
+function updateScreenContent() {
+  screenBottomRow.textContent = inputArray.join("");
+}
+
+function appendInput(input) {
+  inputArray.push(input);
+  updateScreenContent();
+}
+
+let inputArray = [];
 
 let operator;
 let num1;
